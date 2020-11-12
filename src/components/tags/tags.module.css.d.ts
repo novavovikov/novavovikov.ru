8634bcf398ef4a8af8cc7d8266a8c8b561c190de
Vladimir Novikov <vladimir.novikov@genestack.com>
2020-11-12 23:18:51 +0300

    tags limit

--- src/components/tags/tags.module.css.d.ts
+++ src/components/tags/tags.module.css.d.ts
@@ -1,3 +1,4 @@
 export const Tags: string;
 export const Tags__item: string;
 export const Tags__item_active: string;
+export const Tags__more: string;
--- src/components/tags/tags.tsx
+++ src/components/tags/tags.tsx
@@ -6,15 +6,22 @@ import { QUERY_PARAM } from '../../constants/queryParams'
 import s from './tags.module.css'
 
 interface Props {
+  limit: number
   tags: string[]
 }
 
-export default function Tags({ tags }: Props) {
+export default function Tags({ limit = 6, tags }: Props) {
   const [selectedTag] = useQueryParam<string>(QUERY_PARAM.tag)
+  const [isVisible, setVisible] = React.useState<boolean>(false)
+
+  const initialTags = React.useMemo(() => tags.slice(0, limit), [tags])
+  const visibleTags = isVisible ? tags : initialTags
+
+  const toggleVisibility = () => setVisible((v) => !v)
 
   return (
     <div className={s.Tags}>
-      {tags.map((tag) => (
+      {visibleTags.map((tag) => (
         <Link
           key={tag}
           className={cn(s.Tags__item, {
@@ -24,6 +31,14 @@ export default function Tags({ tags }: Props) {
           #{tag}
         </Link>
       ))}
+
+      {tags.length > initialTags.length && (
+        <button
+          className={cn(s.Tags__item, s.Tags__more)}
+          onClick={toggleVisibility}>
+          {isVisible ? 'Скрыть' : 'Показать все'}
+        </button>
+      )}
     </div>
   )
 }
